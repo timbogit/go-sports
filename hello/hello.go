fd1766480a9d3edd3a55bc835f1bd2227d83933b
Tim Schmelmer <tim.schmelmer@gmail.com>
2013-08-18 18:19:06 -0600

    adding some random text to the bottom of the form

--- hello/hello.go
+++ hello/hello.go
@@ -1,36 +1,57 @@
 package hello
 
 import (
-    "appengine"
-    "appengine/datastore"
-    "appengine/user"
-    "html/template"
-    "net/http"
-    "time"
+	"appengine"
+	"appengine/datastore"
+	"appengine/user"
+	"fmt"
+	"html/template"
+	"math/rand"
+	"net/http"
+	"time"
 )
 
 type Greeting struct {
-    Author  string
-    Content string
-    Date    time.Time
+	Author  string
+	Content string
+	Date    time.Time
 }
 
 func init() {
-    http.HandleFunc("/", root)
-    http.HandleFunc("/sign", sign)
+	http.HandleFunc("/", root)
+	http.HandleFunc("/sign", sign)
 }
 
 func root(w http.ResponseWriter, r *http.Request) {
-    c := appengine.NewContext(r)
-    q := datastore.NewQuery("Greeting").Order("-Date").Limit(10)
-    greetings := make([]Greeting, 0, 10)
-    if _, err := q.GetAll(c, &greetings); err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    if err := guestbookTemplate.Execute(w, greetings); err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }
+	c := appengine.NewContext(r)
+	q := datastore.NewQuery("Greeting").Order("-Date").Limit(10)
+	greetings := make([]Greeting, 0, 10)
+	if _, err := q.GetAll(c, &greetings); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err := guestbookTemplate.Execute(w, greetings); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	rc := make(chan string)
+	rand.Seed(time.Now().Unix())
+
+	go func() {
+		time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+		rc <- "random 1"
+	}()
+
+	go func() {
+		time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+		rc <- "random 2"
+	}()
+
+	str := "empty"
+	select {
+	case str = <-rc:
+	}
+	fmt.Fprint(w, str)
 }
 
 var guestbookTemplate = template.Must(template.New("book").Parse(guestbookTemplateHTML))
@@ -55,19 +76,18 @@ const guestbookTemplateHTML = `
 `
 
 func sign(w http.ResponseWriter, r *http.Request) {
-    c := appengine.NewContext(r)
-    g := Greeting{
-        Content: r.FormValue("content"),
-        Date:    time.Now(),
-    }
-    if u := user.Current(c); u != nil {
-        g.Author = u.String()
-    }
-    _, err := datastore.Put(c, datastore.NewIncompleteKey(c, "Greeting", nil), &g)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    http.Redirect(w, r, "/", http.StatusFound)
+	c := appengine.NewContext(r)
+	g := Greeting{
+		Content: r.FormValue("content"),
+		Date:    time.Now(),
+	}
+	if u := user.Current(c); u != nil {
+		g.Author = u.String()
+	}
+	_, err := datastore.Put(c, datastore.NewIncompleteKey(c, "Greeting", nil), &g)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/", http.StatusFound)
 }
-
